48767e0192d343fd6f0864c15ca42ab354549c75
小若憲史 <kowakakenji@kowakaknjinoMBP.lan>
2024-03-16 19:53:44 +0900

    MVP投票

--- test2.rb
+++ test2.rb
@@ -1,17 +1,35 @@
-# N人の数を入力
-n = gets.to_i
+#ナ・リーグのMVP投票
+# 記者の変数
+writer = gets.to_i
 
 # 名前と投票数を格納するハッシュマップ
 votes = Hash.new(0)
 
 # N回のループで各投票を処理
-n.times do
-  name = gets.chomp
-  votes[name] += 1
+writer.times do
+  most_value_player = gets.chomp
+  votes[most_value_player] += 1
 end
-
 
 # 最も得票数が多い人の名前を見つけて出力
 # max_byメソッドはそのメソッドの最大値を評価するメソッドになる
 max_votes = votes.max_by { |key, value| value }
-puts "今回の候補者は#{max_votes[0]}です"
+puts "今年のMVPは#{max_votes[0]}です"
+
+#ア・リーグのMVP投票
+# 記者の変数
+writer = gets.to_i
+
+# 名前と投票数を格納するハッシュマップ
+votes = Hash.new(0)
+
+# N回のループで各投票を処理
+writer.times do
+  most_value_player = gets.chomp
+  votes[most_value_player] += 1
+end
+
+# 最も得票数が多い人の名前を見つけて出力
+# max_byメソッドはそのメソッドの最大値を評価するメソッドになる
+max_votes = votes.max_by { |key, value| value }
+puts "今年のMVPは#{max_votes[0]}です"
